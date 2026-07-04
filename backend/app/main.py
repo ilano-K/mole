081daa4872b3395ff5383c4db94294607a3b590e
Karl <karlchristian8000@gmail.com>
2026-07-04 21:36:22 +0800

    added status to main router

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from app.api.documents import router as documents_router
 from app.api.config import router as config_router
+from app.api.status import router as status_router
 from app.core.exceptions import AppException
 from app.database.database import Base, engine
 
@@ -19,6 +20,7 @@ app.add_middleware(
 
 app.include_router(documents_router)
 app.include_router(config_router)
+app.include_router(status_router)
 
 @app.exception_handler(AppException)
 def app_exception_handler(req: Request, exc: AppException):
