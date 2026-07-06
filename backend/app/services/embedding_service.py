d64ba17c09f33ed246f4f72f86b999672ce44eb3
Karl <karlchristian8000@gmail.com>
2026-07-06 13:52:07 +0800

    added caching of embedding function

--- backend/app/services/embedding_service.py
+++ backend/app/services/embedding_service.py
@@ -4,13 +4,36 @@ from chromadb.utils.embedding_functions import (
     SentenceTransformerEmbeddingFunction, OpenAIEmbeddingFunction,
     OllamaEmbeddingFunction,
 )
+
+_cached_embedding = None 
+_cached_config = None 
+
 def get_embedding_function(config: AppConfig):
+    global _cached_config, _cached_embedding
+    
+    key = (
+        config.embedding_provider,
+        config.embedding_model,
+        config.api_key
+    )
+    
+    # COMPARE FETCHED CONFIG AND CACHED CONFIG
+    if key != _cached_config:
+        # Create embedding function if key and cache doesn't match
+        _cached_embedding = create_embedding(config)
+        # update the cache
+        _cached_config = key 
+    
+    return _cached_embedding
+
+
+def create_embedding(config: AppConfig):
     embedding_model = config.embedding_model
     if config.embedding_provider == EmbeddingProvider.OLLAMA:
         return OllamaEmbeddingFunction(model_name=embedding_model)
     
     if config.embedding_provider == EmbeddingProvider.CLOUD:
-        return OpenAIEmbeddingFunction(model_name=embedding_model)
+        return OpenAIEmbeddingFunction(model_name=embedding_model, api_key=config.api_key)
     
     return SentenceTransformerEmbeddingFunction(model_name=embedding_model)
     
